270139e1dc725e5173ba31f654cff69209f172f5
mppatterson84 <mppatterson84@gmail.com>
2021-06-19 14:00:02 -0500

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php get_template_part('template-parts/content', 'front-page'); ?>
+    <?php endwhile;
+else : ?>
+    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<?php
+get_footer();
+?>
