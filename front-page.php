71f3add6f1deee79caedc1f16d9337f7f3b50b5e
mppatterson84 <mppatterson84@gmail.com>
2021-06-20 22:15:06 -0500

    if homepage displays latest posts, use home content template

--- front-page.php
+++ front-page.php
@@ -2,11 +2,30 @@
 get_header();
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php get_template_part('template-parts/content', 'front-page'); ?>
-    <?php endwhile;
-else : ?>
-    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+<?php if (is_home()) : ?>
+
+    <div class="col-md-6">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php get_template_part('template-parts/content', 'home'); ?>
+            <?php endwhile;
+        else : ?>
+            <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </div>
+
+    <div class="order-4">
+        <?php bs5_the_posts_pagination() ?>
+    </div>
+
+<?php else : ?>
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php get_template_part('template-parts/content', 'front-page'); ?>
+        <?php endwhile;
+    else : ?>
+        <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+
 <?php endif; ?>
 
 <?php
