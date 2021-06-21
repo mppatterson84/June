e36e249c12d878fe101872a6f44d51ddb9ad2a54
mppatterson84 <mppatterson84@gmail.com>
2021-06-21 06:15:32 -0500

    add container class to footer sidebar

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,4 +1,4 @@
-<div id="sidebar-footer" class="sidebar">
+<div id="sidebar-footer" class="sidebar container">
     <?php if (is_active_sidebar('footer')) : ?>
         <?php dynamic_sidebar('footer'); ?>
     <?php else : ?>
