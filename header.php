2503bfb289678346d6239e30b3d0c6daed7685fd
mppatterson84 <mppatterson84@gmail.com>
2021-06-22 11:21:47 -0500

    add search

--- header.php
+++ header.php
@@ -67,13 +67,10 @@
                 ));
                 ?>
 
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success" type="submit">
-                        Search
-                    </button>
-                </form>
-                <!-- < ?php get_search_form(); ?> -->
+                <div>
+                    <?php get_search_form(); ?>
+                </div>
+
             </div>
         </div>
     </nav>
---
+++ search.php
@@ -0,0 +1,22 @@
+<?php
+get_header();
+?>
+
+
+<div class="col-md-6">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php get_template_part('template-parts/content', 'home'); ?>
+        <?php endwhile;
+    else : ?>
+        <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+</div>
+
+<div class="order-4">
+    <?php bs5_the_posts_pagination() ?>
+</div>
+
+
+<?php
+get_footer();
+?>
