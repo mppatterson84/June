8d369e5461c6280ace231dfd3fdb84acf99989f8
mppatterson84 <mppatterson84@gmail.com>
2021-07-12 09:13:58 -0500

    Update single.php

--- single.php
+++ single.php
@@ -2,14 +2,14 @@
 get_header();
 ?>
 
-<div class="col-md-6">
+<main class="col-md-6">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <?php get_template_part('template-parts/content', 'single'); ?>
         <?php endwhile;
     else : ?>
         <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
     <?php endif; ?>
-</div>
+</main>
 
 <?php
 get_footer();
